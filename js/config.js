d4eb575d938dfbec16e1b72e15de35c3ef464bbf
easola1 <easola1@student.gsu.edu>
2025-12-04 10:29:08 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,10 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://rvg8v3z4x1.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://0tqvgck2hk.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
