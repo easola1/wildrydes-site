3c9a59e881a0d043c63b2af8f7c5a749aebf2704
easola1 <easola1@student.gsu.edu>
2025-12-04 10:24:08 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_uw0SEnMb6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '74rnlf0ig4gbls9m2noo3nl7nc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_OCLMajfwG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2bntmuhbdo7jvjr1nr9s1md4a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://rvg8v3z4x1.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
